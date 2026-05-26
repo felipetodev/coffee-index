303a249b99c8bcbaa17b24dc22343284e1c0f5b7
felipetodev <fe.ossandon.u@gmail.com>
2026-05-26 15:39:18 -0400

    feat(auth): localize Clerk components

--- components/auth-provider.tsx
+++ components/auth-provider.tsx
@@ -1,11 +1,12 @@
 "use client"
 
 import { ClerkProvider } from "@clerk/nextjs"
+import { esES } from "@clerk/localizations"
 
 export function AuthProvider({ children }: { children: React.ReactNode }) {
   if (!process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY) {
     return children
   }
 
-  return <ClerkProvider>{children}</ClerkProvider>
+  return <ClerkProvider localization={esES}>{children}</ClerkProvider>
 }
